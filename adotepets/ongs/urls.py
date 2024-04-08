3a0a8f3a40ea26aaccda2b1a7d42100ee75e8d6e
Marsango <gmarsango@gmail.com>
2024-04-08 13:02:11 -0300

    Release 1.0.1

--- adotepets/ongs/urls.py
+++ adotepets/ongs/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
+    # ex: /polls/5/
+    path("<str:ong_name>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<str:ong_name>/pets/", views.pets_available, name="pets_available"),
+    # ex: /polls/5/vote/
+    path("<str:ong_name>/pets/<int:pet_id>", views.pet_x, name="pet_x"),
 ]
--- adotepets/ongs/views.py
+++ adotepets/ongs/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
-
+from .models import ONG, Pet
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the ong index.")
+    all_ongs_list = ONG.objects.all()
+    context = { "all_ongs_list": all_ongs_list }
+    return render(request, "ongs/index.html", context)
+def detail(request, ong_name):
+    ong = get_object_or_404(ONG, ong_name=ong_name)
+    return render(request, "ongs/detail.html", {"ong" : ong})
+
+
+def pets_available(request, ong_name):
+    ong = get_object_or_404(ONG, ong_name = ong_name)
+    pets = get_list_or_404(Pet, ong_id = ong.id)
+    return render(request, "ongs/pets_available.html", {"pets" : pets})
+
+
+def pet_x(request, ong_name, pet_id):
+    pet = get_object_or_404(Pet, id = pet_id)
+    return HttpResponse("Está visualizando a página do pet %s." % pet.pet_name)
